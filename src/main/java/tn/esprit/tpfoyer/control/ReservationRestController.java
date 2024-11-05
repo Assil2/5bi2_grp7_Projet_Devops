b5fa060b73f2083cbb12d288332493267c824c89
Yasmine BEN LARBI <yasmine.benlarbi@esprit.tn>
2024-11-05 21:07:26 +0100

    main - Added loggings to "ReservationRestController.Java"

--- src/main/java/tn/esprit/tpfoyer/control/ReservationRestController.java
+++ src/main/java/tn/esprit/tpfoyer/control/ReservationRestController.java
@@ -33,13 +33,46 @@ public class ReservationRestController {
     @GetMapping("/retrieve-reservation/{reservation-id}")
     public Reservation retrieveReservation(@PathVariable("reservation-id") String rId) {
         logger.info("Request to retrieve reservation with ID: {}", rId);
-        try {
-            Reservation reservation = reservationService.retrieveReservation(rId);
-            logger.debug("Reservation retrieved: {}", reservation);
-            return reservation;
-        } catch (Exception e) {
-            logger.error("Error retrieving reservation with ID: {}", rId, e);
-            throw e;
-        }
+        Reservation reservation = reservationService.retrieveReservation(rId);
+        logger.debug("Reservation retrieved: {}", reservation);
+        return reservation;
     }
+
+    // Retrieve reservations by date and status
+    @GetMapping("/retrieve-reservation-date-status/{d}/{v}")
+    public List<Reservation> retrieveReservationParDateEtStatus(
+            @PathVariable("d") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date d, 
+            @PathVariable("v") boolean b) {
+        logger.info("Request to retrieve reservations by date {} and status {}", d, b);
+        List<Reservation> reservations = reservationService.trouverResSelonDateEtStatus(d, b);
+        logger.debug("Reservations retrieved for date {} and status {}: {}", d, b, reservations);
+        return reservations;
+    }
+
+    // http://localhost:8089/tpfoyer/reservation/add-reservation
+    @PostMapping("/add-reservation")
+    public Reservation addReservation(@RequestBody Reservation r) {
+        logger.info("Request to add a new reservation");
+        Reservation reservation = reservationService.addReservation(r);
+        logger.debug("New reservation added: {}", reservation);
+        return reservation;
+    }
+
+    // http://localhost:8089/tpfoyer/reservation/remove-reservation/{reservation-id}
+    @DeleteMapping("/remove-reservation/{reservation-id}")
+    public void removeReservation(@PathVariable("reservation-id") String rId) {
+        logger.info("Request to remove reservation with ID: {}", rId);
+        reservationService.removeReservation(rId);
+        logger.debug("Reservation with ID {} removed", rId);
+    }
+
+    // http://localhost:8089/tpfoyer/reservation/modify-reservation
+    @PutMapping("/modify-reservation")
+    public Reservation modifyReservation(@RequestBody Reservation r) {
+        logger.info("Request to modify reservation");
+        Reservation reservation = reservationService.modifyReservation(r);
+        logger.debug("Reservation modified: {}", reservation);
+        return reservation;
+    }
+
 }
