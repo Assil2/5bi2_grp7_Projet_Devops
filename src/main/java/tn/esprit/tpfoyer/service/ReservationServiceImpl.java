5dc595219b46fe3d87cf9011e16acd152dfc361e
Yasmine BEN LARBI <yasmine.benlarbi@esprit.tn>
2024-11-13 14:03:21 +0100

    added logs for the service reservation

--- src/main/java/tn/esprit/tpfoyer/service/ReservationServiceImpl.java
+++ src/main/java/tn/esprit/tpfoyer/service/ReservationServiceImpl.java
@@ -1,41 +1,71 @@
 package tn.esprit.tpfoyer.service;
 
-
 import lombok.AllArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 import tn.esprit.tpfoyer.entity.Reservation;
 import tn.esprit.tpfoyer.repository.ReservationRepository;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class ReservationServiceImpl implements IReservationService {
 
-    ReservationRepository reservationRepository;
+    private static final Logger logger = LogManager.getLogger(ReservationServiceImpl.class);
+
+    private final ReservationRepository reservationRepository;
 
     public List<Reservation> retrieveAllReservations() {
-        return reservationRepository.findAll();
+        logger.info("Starting retrieval of all reservations");
+        List<Reservation> reservations = reservationRepository.findAll();
+        logger.debug("Reservations retrieved: {}", reservations);
+        return reservations;
     }
 
     public Reservation retrieveReservation(String reservationId) {
-        return reservationRepository.findById(reservationId).get();
+        logger.info("Starting retrieval of reservation with ID: {}", reservationId);
+        Optional<Reservation> reservation = reservationRepository.findById(reservationId);
+        if (reservation.isPresent()) {
+            logger.debug("Reservation retrieved: {}", reservation.get());
+            return reservation.get();
+        } else {
+            logger.error("Reservation with ID {} not found", reservationId);
+            return null; // ou lance une exception si tu préfères
+        }
     }
 
     public Reservation addReservation(Reservation r) {
-        return reservationRepository.save(r);
+        logger.info("Adding a new reservation");
+        Reservation savedReservation = reservationRepository.save(r);
+        logger.debug("New reservation added: {}", savedReservation);
+        return savedReservation;
     }
 
     public Reservation modifyReservation(Reservation reservation) {
-        return reservationRepository.save(reservation);
+        logger.info("Modifying reservation with ID: {}", reservation.getIdReservation());
+        Reservation updatedReservation = reservationRepository.save(reservation);
+        logger.debug("Reservation modified: {}", updatedReservation);
+        return updatedReservation;
     }
 
     public List<Reservation> trouverResSelonDateEtStatus(Date d, boolean b) {
-        return reservationRepository.findAllByAnneeUniversitaireBeforeAndEstValide(d, b);
+        logger.info("Retrieving reservations by date {} and status {}", d, b);
+        List<Reservation> reservations = reservationRepository.findAllByAnneeUniversitaireBeforeAndEstValide(d, b);
+        logger.debug("Reservations found for date {} and status {}: {}", d, b, reservations);
+        return reservations;
     }
 
     public void removeReservation(String reservationId) {
-        reservationRepository.deleteById(reservationId);
+        logger.info("Removing reservation with ID: {}", reservationId);
+        try {
+            reservationRepository.deleteById(reservationId);
+            logger.debug("Reservation with ID {} successfully removed", reservationId);
+        } catch (Exception e) {
+            logger.error("Error removing reservation with ID {}", reservationId, e);
+        }
     }
 }
